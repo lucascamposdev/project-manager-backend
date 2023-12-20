6d35a211e297718864177517bcb5b47efbaff390
Lucas <lucaskmps@gmail.com>
2023-12-20 17:29:01 -0300

    correção de conexão com elephantsql

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,15 +1,9 @@
 import Sequelize from 'sequelize';
 
-const DB_NAME = process.env.DB_NAME
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
 const DB_URL = process.env.DB_URL
 
-const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    host: DB_URL,
-    dialect: 'postgres',
+const connection = new Sequelize(DB_URL, {
     logging: false,
-    port: 5432
 })
 
 connection.authenticate()
