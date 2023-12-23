d2213d7f73ee77768c846952b6397a07e2ab3880
Lucas <lucaskmps@gmail.com>
2023-12-23 14:55:04 -0300

    cors adjuts 2.0

--- server.js
+++ server.js
@@ -9,13 +9,7 @@ const PORT = process.env.PORT || 9001
 // Cors
 import cors from 'cors';
 
-const corsOptions = {
-    origin: 'https://project-manager-frontend.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, 
-  };
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Routes
 import router from './app/routes/Routes.js'
