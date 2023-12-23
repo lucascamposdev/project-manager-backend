83712c844cc9ac7b324dc1a7cd0512cbd2a58449
Lucas <lucaskmps@gmail.com>
2023-12-23 15:15:19 -0300

    cors adjustment 2.0

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import express from "express";
 const app = express();
+
+// Cors
+import cors from 'cors';
+app.use(cors());
+
 app.use(express.json());
 
 // .ENV
 import "dotenv/config.js";
 const PORT = process.env.PORT || 9001
-
-// Cors
-import cors from 'cors';
-
-app.use(cors());
 
 // Routes
 import router from './app/routes/Routes.js'
